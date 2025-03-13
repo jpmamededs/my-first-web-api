bcb687d827e4494d14e8f739866b7640dbd1d716
João Pedro Mamede <joaopedrodias@live.com>
2025-03-13 08:45:16 -0300

    Start project

---
+++ src/test/java/mamede/my_first_web_api/MyFirstWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mamede.my_first_web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
